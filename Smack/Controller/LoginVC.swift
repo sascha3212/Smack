0ca2c4e595a0037ac8f8af44cd88a1832ca5d47b
Sascha Landegge <saschalandegge@live.nl>
2018-01-13 22:05:33 +0100

    added login controller and view

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Sascha Landegge on 13/01/2018.
+//  Copyright © 2018 Codeunicum. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Sascha Landegge on 13/01/2018.
+//  Copyright © 2018 Codeunicum. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
