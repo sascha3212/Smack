eee52a53c67575fd58d02b3a3270dd0f2a56dda6
Sascha Landegge <saschalandegge@live.nl>
2018-01-16 13:57:07 +0100

    added API call to register

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Succes: Bool) -> ()
+
+//URL
+let BASE_URL = "https://chattychatsaschal.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
